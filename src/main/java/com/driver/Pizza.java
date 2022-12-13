98615bc720be824a4a174da6c95d87c1a6f86f82
devanggg123 <114430661+devanggg123@users.noreply.github.com>
2022-12-13 20:44:18 +0530

    Update Pizza.java

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,17 +5,21 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean ec=false;
+    private boolean et=false;
+    private boolean cb=false;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
         if(this.isVeg) {
             this.price = 300;
-            System.out.println("Base Price Of The Pizza: 300");
+
         }
         else {
             this.price = 400;
-            System.out.println("Base Price Of The Pizza: 400");
+
         }
 
     }
@@ -26,32 +30,48 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        System.out.println("Extra Cheese Added: 80");
+        this.ec=true;
+
         this.price+=80;
 
     }
 
     public void addExtraToppings(){
         // your code goes here
+        this.et=true;
         if(this.isVeg) {
-            System.out.println("Extra Toppings Added: 70");
+
             this.price += 70;
         }
         else {
             this.price += 120;
-            System.out.println("Extra Toppings Added: 120");
+
         }
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.cb=true;
         this.price+=20;
-        System.out.println("Paperbag Added: 20");
+
     }
 
     public String getBill(){
         // your code goes here
-        this.bill="Total Price: "+this.price;
+        if(isVeg)
+            this.bill="Base Price Of The Pizza: 300"+"\n";
+        else this.bill="Base Price Of The Pizza: 400"+"\n";
+
+        if(this.ec)
+            this.bill+="Extra Cheese Added: 80"+"\n";
+        if(this.et==true&&this.isVeg==true)
+            this.bill+="Extra Toppings Added: 70"+"\n";
+        else if(this.et==true&&this.isVeg==false)
+            this.bill+="Extra Toppings Added: 120"+"\n";
+
+        if(cb)
+            this.bill+="Paperbag Added: 20"+"\n";
+        this.bill+="Total Price: "+this.price;
         return this.bill;
     }
 }
